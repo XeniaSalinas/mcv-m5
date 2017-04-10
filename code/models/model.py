3594721a40f2095bc214521a0f8a3890a4da14e4
LidiaGarrucho <lidiaxu.3@gmail.com>
2017-04-10 22:26:39 +0200

    Fix predict (not finished) and return the metrics if ensamble is true

--- code/models/model.py
+++ code/models/model.py
@@ -7,6 +7,8 @@ from keras.engine.training import GeneratorEnqueuer
 from tools.save_images import save_img3
 from tools.yolo_utils import *
 from keras.preprocessing import image
+from tools.optimizer_factory import Optimizer_Factory
+
 """
 Interface for normal (one net) models and adversarial models. Objects of
 classes derived from Model are returned by method make() of the Model_Factory
@@ -61,8 +63,8 @@ class One_Net_Model(Model):
     # Predict the model
     def predict(self, test_gen, tag='pred'):
         if self.cf.pred_model:
-            print('Predict method not implemented.')
-            return
+            # print('Predict method not implemented.')
+            # return
             # TODO fix model predict method
             print('\n > Predicting the model...')
             # Load best trained model
@@ -70,7 +72,8 @@ class One_Net_Model(Model):
             self.model.load_weights(self.cf.weights_file)
 
             # Create a data generator
-            data_gen_queue, _stop, _generator_threads = GeneratorEnqueuer(self.test_gen, max_q_size=1)
+            enqueuer = GeneratorEnqueuer(test_gen)
+            enqueuer.start(max_q_size=1, nb_worker=1)
 #            data_gen_queue, _stop, _generator_threads = GeneratorEnqueuer(test_gen, max_q_size=1)
 
             # Process the dataset
@@ -79,9 +82,16 @@ class One_Net_Model(Model):
             for _ in range(int(math.ceil(self.cf.dataset.n_images_test/float(self.cf.batch_size_test)))):
 
                 # Get data for this minibatch
-                data = data_gen_queue.get()
-                x_true = data[0]
-                y_true = data[1].astype('int32')
+                # data = enqueuer.queue.get()
+                while enqueuer.is_running():
+                    if not enqueuer.queue.empty():
+                        generator_output = enqueuer.queue.get()
+                        break
+                    else:
+                        time.sleep(0.01)
+
+                x_true = generator_output[0]
+                y_true = generator_output[1].astype('int32')
 
                 # Get prediction for this minibatch
                 y_pred = self.model.predict(x_true)
@@ -89,15 +99,16 @@ class One_Net_Model(Model):
                 # Compute the argmax
                 y_pred = np.argmax(y_pred, axis=1)
 
-                # Reshape y_true
-                y_true = np.reshape(y_true, (y_true.shape[0], y_true.shape[2],
-                                             y_true.shape[3]))
-
-                save_img3(x_true, y_true, y_pred, self.cf.savepath, 0,
-                          self.cf.dataset.color_map, self.cf.dataset.classes, tag+str(_), self.cf.dataset.void_class)
+                # # Reshape y_true
+                # y_true = np.reshape(y_true, (y_true.shape[0], y_true.shape[2],
+                #                              y_true.shape[3]))
+                #
+                # save_img3(x_true, y_true, y_pred, self.cf.savepath, 0,
+                #           self.cf.dataset.color_map, self.cf.dataset.classes, tag+str(_), self.cf.dataset.void_class)
 
             # Stop data generator
-            _stop.set()
+            if enqueuer is not None:
+                enqueuer.stop()
 
             total_time = time.time() - start_time
             fps = float(self.cf.dataset.n_images_test) / total_time
@@ -105,112 +116,131 @@ class One_Net_Model(Model):
             print ('   Predicting time: {}. FPS: {}. Seconds per Frame: {}'.format(total_time, fps, s_p_f))
 
     # Test the model
-    def test(self, test_gen):
+    def test(self, test_gen, model_ensemble=False, weights_file_2=None):
         if self.cf.test_model:
             print('\n > Testing the model...')
-            # Load best trained model
-            print('   loading model weights from: ' + self.cf.weights_test_file + '...')
-            self.model.load_weights(self.cf.weights_test_file)
 
-            # Evaluate model
-            start_time_global = time.time()
-            test_metrics = self.model.evaluate_generator(test_gen,
-                                                         self.cf.dataset.n_images_test,
-                                                         max_q_size=10,
-                                                         nb_worker=1,
-                                                         pickle_safe=False)
-            if self.cf.problem_type == 'detection':
-                # Dataset and the model used
-                dataset_name = self.cf.dataset_name 
-                #model_name = self.cf.model_name 
-                # Net output post-processing needs two parameters:
-                detection_threshold = 0.6 # Min probablity for a prediction to be considered
-                nms_threshold       = 0.2 # Non Maximum Suppression threshold
-                # IMPORTANT: the values of these two params will affect the final performance of the netwrok
-                #            you are allowed to find their optimal values in the validation/train sets
-                
-                if dataset_name == 'TT100K_detection':
-                    classes = ['i2','i4','i5','il100','il60','il80','io','ip','p10','p11','p12','p19','p23','p26','p27','p3','p5','p6','pg','ph4','ph4.5','ph5','pl100','pl120','pl20','pl30','pl40','pl5','pl50','pl60','pl70','pl80','pm20','pm30','pm55','pn','pne','po','pr40','w13','w32','w55','w57','w59','wo']
-                elif dataset_name == 'Udacity':
-                    classes = ['Car','Pedestrian','Truck']
-                else:
-                    print "Error: Dataset not found!"
-                    quit()
-                priors = [[0.9,1.2], [1.05,1.35], [2.15,2.55], [3.25,3.75], [5.35,5.1]]
-                
-                input_shape = (self.cf.dataset.n_channels,
-                        self.cf.target_size_test[0],
-                        self.cf.target_size_test[1])
-                
+            if model_ensemble == True:
+                # Load best trained model
+                if hasattr(self.cf, 'model_name_2'):
+                    if weights_file_2 != None:
+                        print('   loading model weights from: ' + weights_file_2 + '...')
+                        self.model.load_weights(weights_file_2)
+                    else:
+                        print('   loading model weights from: ' + self.cf.weights_test_file + '...')
+                        self.model.load_weights(self.cf.weights_test_file)
 
-                
-                test_dir = test_gen.directory
-                imfiles = [os.path.join(test_dir,f) for f in os.listdir(test_dir) 
-                                    if os.path.isfile(os.path.join(test_dir,f)) 
-                                    and f.endswith('jpg')]
-                inputs = []
-                img_paths = []
-                chunk_size = 128 # we are going to process all image files in chunks
-                
-                ok = 0.
-                total_true = 0.
-                total_pred = 0.
-                for i, img_path in enumerate(imfiles):
-                    img = image.load_img(img_path, target_size=(input_shape[1], input_shape[2]))
-                    img = image.img_to_array(img)
-                    img = img / 255.
-                    inputs.append(img.copy())
-                    img_paths.append(img_path)
-                    
-                    if len(img_paths)%chunk_size == 0 or i+1 == len(imfiles):
-                        inputs = np.array(inputs)
-                        start_time_batch = time.time()
-                        net_out = self.model.predict(inputs, batch_size = 16, verbose = 1)
-                        print ('{} images predicted in {:.5f} seconds. {:.5f} fps').format(len(inputs), 
-                               time.time() - start_time_batch, 
-                                (len(inputs)/(time.time() - start_time_batch)))
-                        # Find correct detections (per image)
-                        for i, img_path in enumerate(img_paths):
-                            boxes_pred = yolo_postprocess_net_out(net_out[i], priors, classes, detection_threshold, nms_threshold)
-                            boxes_true = []
-                            label_path = img_path.replace('jpg','txt')
-                            gt = np.loadtxt(label_path)
-                            if len(gt.shape) == 1:
-                                gt = gt[np.newaxis,]
-                            for j in range(gt.shape[0]):
-                                bx = BoundBox(len(classes))
-                                bx.probs[int(gt[j,0])] = 1.
-                                bx.x, bx.y, bx.w, bx.h = gt[j,1:].tolist()
-                                boxes_true.append(bx)
-                            
-                            total_true += len(boxes_true)
-                            true_matched = np.zeros(len(boxes_true))
-                            for b in boxes_pred:
-                                if b.probs[np.argmax(b.probs)] < detection_threshold:
-                                    continue
-                                total_pred += 1.
-                                for t,a in enumerate(boxes_true):
-                                    if true_matched[t]:
+                # Evaluate model
+                test_metrics = self.model.evaluate_generator(test_gen,
+                                                        self.cf.dataset.n_images_test,
+                                                        max_q_size=10,
+                                                        nb_worker=1,
+                                                        pickle_safe=False)
+                return test_metrics, self.model.metrics_names
+            else:
+                # Load best trained model
+                print('   loading model weights from: ' + self.cf.weights_test_file + '...')
+                self.model.load_weights(self.cf.weights_test_file)
+
+                # Evaluate model
+                start_time_global = time.time()
+                test_metrics = self.model.evaluate_generator(test_gen,
+                                                             self.cf.dataset.n_images_test,
+                                                             max_q_size=10,
+                                                             nb_worker=1,
+                                                             pickle_safe=False)
+                if self.cf.problem_type == 'detection':
+                    # Dataset and the model used
+                    dataset_name = self.cf.dataset_name
+                    #model_name = self.cf.model_name
+                    # Net output post-processing needs two parameters:
+                    detection_threshold = 0.6 # Min probablity for a prediction to be considered
+                    nms_threshold       = 0.2 # Non Maximum Suppression threshold
+                    # IMPORTANT: the values of these two params will affect the final performance of the netwrok
+                    #            you are allowed to find their optimal values in the validation/train sets
+
+                    if dataset_name == 'TT100K_detection':
+                        classes = ['i2','i4','i5','il100','il60','il80','io','ip','p10','p11','p12','p19','p23','p26','p27','p3','p5','p6','pg','ph4','ph4.5','ph5','pl100','pl120','pl20','pl30','pl40','pl5','pl50','pl60','pl70','pl80','pm20','pm30','pm55','pn','pne','po','pr40','w13','w32','w55','w57','w59','wo']
+                    elif dataset_name == 'Udacity':
+                        classes = ['Car','Pedestrian','Truck']
+                    else:
+                        print "Error: Dataset not found!"
+                        quit()
+                    priors = [[0.9,1.2], [1.05,1.35], [2.15,2.55], [3.25,3.75], [5.35,5.1]]
+
+                    input_shape = (self.cf.dataset.n_channels,
+                            self.cf.target_size_test[0],
+                            self.cf.target_size_test[1])
+
+
+
+                    test_dir = test_gen.directory
+                    imfiles = [os.path.join(test_dir,f) for f in os.listdir(test_dir)
+                                        if os.path.isfile(os.path.join(test_dir,f))
+                                        and f.endswith('jpg')]
+                    inputs = []
+                    img_paths = []
+                    chunk_size = 128 # we are going to process all image files in chunks
+
+                    ok = 0.
+                    total_true = 0.
+                    total_pred = 0.
+                    for i, img_path in enumerate(imfiles):
+                        img = image.load_img(img_path, target_size=(input_shape[1], input_shape[2]))
+                        img = image.img_to_array(img)
+                        img = img / 255.
+                        inputs.append(img.copy())
+                        img_paths.append(img_path)
+
+                        if len(img_paths)%chunk_size == 0 or i+1 == len(imfiles):
+                            inputs = np.array(inputs)
+                            start_time_batch = time.time()
+                            net_out = self.model.predict(inputs, batch_size = 16, verbose = 1)
+                            print ('{} images predicted in {:.5f} seconds. {:.5f} fps').format(len(inputs),
+                                   time.time() - start_time_batch,
+                                    (len(inputs)/(time.time() - start_time_batch)))
+                            # Find correct detections (per image)
+                            for i, img_path in enumerate(img_paths):
+                                boxes_pred = yolo_postprocess_net_out(net_out[i], priors, classes, detection_threshold, nms_threshold)
+                                boxes_true = []
+                                label_path = img_path.replace('jpg','txt')
+                                gt = np.loadtxt(label_path)
+                                if len(gt.shape) == 1:
+                                    gt = gt[np.newaxis,]
+                                for j in range(gt.shape[0]):
+                                    bx = BoundBox(len(classes))
+                                    bx.probs[int(gt[j,0])] = 1.
+                                    bx.x, bx.y, bx.w, bx.h = gt[j,1:].tolist()
+                                    boxes_true.append(bx)
+
+                                total_true += len(boxes_true)
+                                true_matched = np.zeros(len(boxes_true))
+                                for b in boxes_pred:
+                                    if b.probs[np.argmax(b.probs)] < detection_threshold:
                                         continue
-                                    if box_iou(a, b) > 0.5 and np.argmax(a.probs) == np.argmax(b.probs):
-                                        true_matched[t] = 1
-                                        ok += 1.
-                                        break
-                            # You can visualize/save per image results with this:
-                            #im = cv2.imread(img_path)
-                            #im = yolo_draw_detections(boxes_pred, im, priors, classes, detection_threshold, nms_threshold)
-                            #cv2.imshow('', im)
-                            #cv2.waitKey(0)
-                        inputs = []
-                        img_paths = []
-                    
-                        #print 'total_true:',total_true,' total_pred:',total_pred,' ok:',ok
-                        p = 0. if total_pred == 0 else (ok/total_pred)
-                        r = ok/total_true
-                        print('Precission = ' + str(p))
-                        print('Recall     = ' + str(r))
-                        f = 0. if (p + r) == 0 else (2*p*r/(p + r))
-                        print('F-score    = '+str(f))
+                                    total_pred += 1.
+                                    for t,a in enumerate(boxes_true):
+                                        if true_matched[t]:
+                                            continue
+                                        if box_iou(a, b) > 0.5 and np.argmax(a.probs) == np.argmax(b.probs):
+                                            true_matched[t] = 1
+                                            ok += 1.
+                                            break
+                                # You can visualize/save per image results with this:
+                                #im = cv2.imread(img_path)
+                                #im = yolo_draw_detections(boxes_pred, im, priors, classes, detection_threshold, nms_threshold)
+                                #cv2.imshow('', im)
+                                #cv2.waitKey(0)
+                            inputs = []
+                            img_paths = []
+
+                            #print 'total_true:',total_true,' total_pred:',total_pred,' ok:',ok
+                            p = 0. if total_pred == 0 else (ok/total_pred)
+                            r = ok/total_true
+                            print('Precission = ' + str(p))
+                            print('Recall     = ' + str(r))
+                            f = 0. if (p + r) == 0 else (2*p*r/(p + r))
+                            print('F-score    = '+str(f))
     
     
             total_time_global = time.time() - start_time_global
